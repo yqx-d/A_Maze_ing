dd76222264a44e83e97e1c43a9ca4852cb5c6347
FOYER Emilien <emilienfoyer@gmail.com>
2026-02-10 14:12:55 +0100

    fix display (42 and path)

--- Maze/display.py
+++ Maze/display.py
@@ -5,40 +5,42 @@ from Maze.solver import solve_maze
 
 class Display:
     @staticmethod
-    def theme_maze(theme: str) -> tuple:
+    def theme_maze(
+        theme: str
+    ) -> tuple:
         entry = "█"
         exit = "█"
         wall = "█"
         path = "█"
-        forthy_two = "█"
+        forty_two = "█"
         space = " "
 
         if theme.upper() == "BLUE":
-            entry = "\033[94m" + entry + "\033[0m"
-            exit = "\033[34m" + exit + "\033[0m"
-            wall = "\033[44m" + wall + "\033[0m"
-            path = "\033[96m" + path + "\033[0m"
-            forthy_two = "\033[94;1m" + forthy_two + "\033[0m"
+            entry = "\033[94;1m" + entry + "\033[0m"
+            exit = "\033[36;1m" + exit + "\033[0m"
+            wall = "\033[34m" + wall + "\033[0m"
+            path = "\033[96;1m" + path + "\033[0m"
+            forty_two = "\033[94;1m" + forty_two + "\033[0m"
         elif theme.upper() == "YELLOW":
-            entry = "\033[93m" + entry + "\033[0m"
-            exit = "\033[33m" + exit + "\033[0m"
-            wall = "\033[43m" + wall + "\033[0m"
-            path = "\033[33;1m" + path + "\033[0m"
-            forthy_two = "\033[93;1m" + forthy_two + "\033[0m"
-        elif theme.upper() == "PURPLE":
-            entry = "\033[95m" + entry + "\033[0m"
-            exit = "\033[35m" + exit + "\033[0m"
-            wall = "\033[45m" + wall + "\033[0m"
-            path = "\033[95;1m" + path + "\033[0m"
-            forthy_two = "\033[35;1m" + forthy_two + "\033[0m"
-        else:
-            entry = "\033[97m" + entry + "\033[0m"
-            exit = "\033[37m" + exit + "\033[0m"
-            wall = "\033[107m" + wall + "\033[0m"
+            entry = "\033[93;1m" + entry + "\033[0m"
+            exit = "\033[91;1m" + exit + "\033[0m"
+            wall = "\033[33m" + wall + "\033[0m"
             path = "\033[97;1m" + path + "\033[0m"
-            forthy_two = "\033[92;1m" + forthy_two + "\033[0m"
+            forty_two = "\033[93;1m" + forty_two + "\033[0m"
+        elif theme.upper() == "PURPLE":
+            entry = "\033[95;1m" + entry + "\033[0m"
+            exit = "\033[94;1m" + exit + "\033[0m"
+            wall = "\033[35m" + wall + "\033[0m"
+            path = "\033[95;1m" + path + "\033[0m"
+            forty_two = "\033[35;1m" + forty_two + "\033[0m"
+        else:
+            entry = "\033[97;1m" + entry + "\033[0m"
+            exit = "\033[37;1m" + exit + "\033[0m"
+            wall = "\033[97m" + wall + "\033[0m"
+            path = "\033[97;1m" + path + "\033[0m"
+            forty_two = "\033[92;1m" + forty_two + "\033[0m"
 
-        return entry, exit, wall, space, path, forthy_two
+        return entry, exit, wall, space, path, forty_two
 
     @staticmethod
     def display_maze(
@@ -68,14 +70,15 @@ class Display:
                 cell = maze[y][x]
 
                 top_line += wall
-
-                if cell.north:
-                    top_line += wall
-                    #ok
-                elif (x, y) in path_to_exit and show_path:
-                    top_line += path
-                elif (x, y) in forty_two_pos:
+                if ((x, y) in forty_two_pos and
+                   (x, y-1) in forty_two_pos):
                     top_line += forthy_two
+                elif cell.north:
+                    top_line += wall
+                elif ((x, y) in path_to_exit and
+                      (x, y-1) in path_to_exit and
+                      show_path):
+                    top_line += path
                 else:
                     top_line += space
             top_line += wall
@@ -84,11 +87,14 @@ class Display:
             mid_line = ""
             for x in range(len(maze[y])):
                 cell = maze[y][x]
-                # Mur ouest ou espace
-                if cell.west:
+                if ((x, y) in forty_two_pos and
+                   (x-1, y) in forty_two_pos):
+                    mid_line += forthy_two
+                elif cell.west:
                     mid_line += wall
-                # retire le prb est mais genere Nord
-                elif (x, y) in path_to_exit and show_path:
+                elif ((x, y) in path_to_exit and
+                      (x-1, y) in path_to_exit and
+                      show_path):
                     mid_line += path
                 else:
                     mid_line += space
@@ -118,12 +124,8 @@ class Display:
             bot_line += wall
             if cell.south:
                 bot_line += wall
-            elif (x, y) in path_to_exit and show_path:
-                bot_line += path
-            elif (x, y) in forty_two_pos:
-                bot_line += forthy_two
             else:
                 bot_line += space
-        # Coin sud-est
+
         bot_line += wall
         print(bot_line)
